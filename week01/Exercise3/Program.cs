fc48b259d0012c5c91e5aafbd5e2bcdfc38bc478
Koua Firmin N'goran <kngoran4@byupathway.edu>
2026-09-01 19:29:00 +0100

    refresh file

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,39 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine();
+
+
+        Console.Write("what is your magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+
+        Random randomGenerator = new Random();
+
+        int magicNumberGenerated = randomGenerator.Next(1, 100);
+
+        int userGuess = -1;
+
+        while (userGuess != magicNumberGenerated)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (userGuess < magicNumber)
+            {
+                Console.WriteLine("Heigher");
+            }
+
+            else if (userGuess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
