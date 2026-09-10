377514a71a5a45e542140c434e16e6f9e6ecae45
Koua Firmin N'goran <kngoran4@byupathway.edu>
2026-09-10 11:13:23 +0100

    Refresh file

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date}");
+        Console.WriteLine($"Prompt: {_promptText}");
+        Console.WriteLine($"Entry: {_entryText}");
+    }
+
+    public string ToFileFormat()
+    {
+        return ($"{_date}|{_promptText}|{_entryText}");
+    }
+
+}
