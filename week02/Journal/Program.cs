766f812f556fb36d6a180e3fd2be6dd31490a14b
Koua Firmin N'goran <kngoran4@byupathway.edu>
2026-09-10 10:36:07 +0100

    refreshfile

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Journal Project.");
+
+
+
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
     }
 }
